b82cc8c61c624655fe2ef1d87013a58ae65996a4
Mohamed reda afellad <mohamedredaafellad@gmail.com>
2026-03-31 19:11:54 +0200

    add

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,11 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
+    
+    'allowed_origins' => ['http://localhost:3000'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+   
 
     'allowed_origins_patterns' => [],
 
@@ -30,5 +32,6 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
+
 
 ];
