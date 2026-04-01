8ebcb82e0f01473e68c9820014492a75d007a7a9
pressinghichamapp-maker <chroudiimran@gmail.com>
2026-04-01 09:09:36 +0100

    change the column email_verified_at from users to laundries

--- backend/app/Http/Controllers/ProviderController.php
+++ backend/app/Http/Controllers/ProviderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Provider;
 use Illuminate\Http\Request;
 
 class ProviderController extends Controller
@@ -38,6 +39,7 @@ class ProviderController extends Controller
         ]);
 
         $provider=Provider::create($request->all());
+        
 
         return response()->json($provider, 201);
     }
