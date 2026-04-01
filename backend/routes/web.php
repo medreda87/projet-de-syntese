76f5cdb8c90dc01c6c39268758a6f01d55580213
Mohamed reda afellad <mohamedredaafellad@gmail.com>
2026-04-01 13:08:05 +0200

    add

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\LaundryController;
+
 use App\Http\Controllers\UserController;
 
-Route::apiResource('products', ProductController::class);
-Route::apiResource('categories', CategoryController::class);
-
-Route::get('/laundry', [LaundryController::class, 'index']);
-Route::post('/laundry', [LaundryController::class, 'store']);
-
-
-Route::resources("/user" , UserController::class);
+Route::resource('users', UserController::class);
